c4611c96d8910611e519601e4f15b2b7c3e4e563
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2019-10-11 20:03:23 +0200

    1. nap 1, 2

--- 2018/1/1-2.py
+++ 2018/1/1-2.py
@@ -1,6 +1,6 @@
 a = 0
 ak = set()
-with open("input (1).e") as be:
+with open(".\\2018\\1\\input (1).e") as be:
     fajl = list(map(int, be.readlines()))
 
 while True:
@@ -10,5 +10,5 @@ while True:
         if a in ak:
             print(a)
             exit()
-        ak = ak | {a}
+        ak.add(a)
 # print(ak)
