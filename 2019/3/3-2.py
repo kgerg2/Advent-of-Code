ac812168999617a852772d985e071f843a407110
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2019-12-05 19:41:12 +0100

    3. nap

--- 2019/3/3-2.py
+++ 2019/3/3-2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open(".\\2019\\3\\input.e") as be:
     l1 = list(map(lambda x: (x[0], int(x[1:])), be.readline().split(',')))
@@ -40,45 +41,51 @@ for ir, h in l2:
 
 print(minx, maxx, miny, maxy)
 
-tabla = np.zeros((maxx-minx+1, maxy-miny+1), dtype=np.int8)
+tabla = np.zeros((maxx-minx+1, maxy-miny+1), dtype=np.int16)
 
 metszetek = []
 
-i = 0
+i = 1
 x1 = y1 = 0
 x2 = y2 = 0
 h1 = h2 = 0
-while l1 and l2:
-    if not h1:
-        ir1, h1 = l1.pop(0)
-    if not h2:
-        ir2, h2 = l2.pop(0)
-    if ir1 == 'R':
-        x1 += 1
-    elif ir1 == 'L':
-        x1 -= 1
-    elif ir1 == 'U':
-        y1 += 1
-    elif ir1 == 'D':
-        y1 -= 1
-    if ir2 == 'R':
-        x2 += 1
-    elif ir2 == 'L':
-        x2 -= 1
-    elif ir2 == 'U':
-        y2 += 1
-    elif ir2 == 'D':
-        y2 -= 1
-    if tabla[x1-minx][y1-miny] > 0:
-        print("most")
-    tabla[x1-minx][y1-miny] += i
-    if tabla[x1-minx][y1-miny] > i:
-        print(tabla[x1-minx][y1-miny])
-        break
-    tabla[x2-minx][y2-miny] += i
-    if tabla[x2-minx][y2-miny] > i:
-        print(tabla[x2-minx][y2-miny])
-        break
-    h1 -= 1
-    h2 -= 1
+while l1 or l2 or h1 or h2:
+    if l1 or h1:
+        if not h1:
+            ir1, h1 = l1.pop(0)
+        if ir1 == 'R':
+            x1 += 1
+        elif ir1 == 'L':
+            x1 -= 1
+        elif ir1 == 'U':
+            y1 += 1
+        elif ir1 == 'D':
+            y1 -= 1
+        if tabla[x1-minx][y1-miny] < 0:
+            metszetek.append(i-tabla[x1-minx][y1-miny])
+        else:
+            tabla[x1-minx][y1-miny] += i
+        h1 -= 1
+    if l2 or h2:
+        if not h2:
+            ir2, h2 = l2.pop(0)
+        if ir2 == 'R':
+            x2 += 1
+        elif ir2 == 'L':
+            x2 -= 1
+        elif ir2 == 'U':
+            y2 += 1
+        elif ir2 == 'D':
+            y2 -= 1
+        if tabla[x2-minx][y2-miny] > 0:
+            metszetek.append(i+tabla[x2-minx][y2-miny])
+        else:
+            tabla[x2-minx][y2-miny] -= i
+        h2 -= 1
     i += 1
+
+print(metszetek)
+print(min(metszetek))
+plt.imshow(tabla, cmap="gray")
+plt.show()
+#print("\n".join(list(map(str, map(list, tabla)))))
